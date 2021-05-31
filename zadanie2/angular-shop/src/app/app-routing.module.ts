7a8eb6fba5ef2d4030168a33484525caaefb66f9
arturgorowski <art.gorowski@gmail.com>
2021-05-31 23:21:48 +0200

    home component; added reflex grid

--- zadanie2/angular-shop/src/app/app-routing.module.ts
+++ zadanie2/angular-shop/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: <any>HomeComponent,
+    children: [
+      // {}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
